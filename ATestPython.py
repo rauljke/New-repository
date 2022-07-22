4b9564bb5538464fa6839f8e835ae65d35079ad8
rauljke <108367279+rauljke@users.noreply.github.com>
2022-07-23 02:02:42 +0300

    Create ATestPython.py

---
+++ ATestPython.py
@@ -0,0 +1,17 @@
+def set_cam_cfg(self, cam_cfg):
+        """
+        In case we need to reconfigure for a different cam cfg
+        :param cam_cfg: the new cam_cfg
+        :return: None
+        """
+        # close the current connections
+        self.mcu.disconnect()
+        self.isp.disconnect()
+        self.mcu = None
+        self.isp = None
+
+        # set new config
+        self.cam_cfg = cam_cfg
+        self.cam_defaults = self.utils.defaults.get_defaults_cam(cam_cfg.get('model'))
+        self.mcu = SerialMCU(self.utils, self.cam_cfg)
+        self.isp = SerialISP(self.utils, self.cam_cfg)
